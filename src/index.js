e2b22ff127a512b199d811d013203dcebe683112
Martin Setaro <86411405+martinsetaro@users.noreply.github.com>
2022-04-06 22:39:07 -0300

    Mi primer proyecto clase 01

---
+++ src/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+
+
+function App() {
+  return "Hola Mundo"
+    
+    
+
+}
+
+ReactDOM.render(<App />, document.getElementById('root'));
